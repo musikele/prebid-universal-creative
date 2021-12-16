cec7535a98273e09766008a23236a10b91ea89c1
Filip Stamenkovic <ficadub@gmail.com>
2021-12-16 13:12:46 +0100

    replace ortb2 with ortb

--- src/nativeAssetManager.js
+++ src/nativeAssetManager.js
@@ -288,8 +288,8 @@ export function newNativeAssetManager(win, pubUrl) {
 
       if ((data.hasOwnProperty('rendererUrl') && data.rendererUrl) || (flag && win.pbNativeData.hasOwnProperty('rendererUrl'))) {
         let renderPayload = data.assets;
-        if (data.ortb2) {
-          renderPayload = data.ortb2;
+        if (data.ortb) {
+          renderPayload = data.ortb;
         }
         if (win.renderAd) {
           const newHtml = (win.renderAd && win.renderAd(renderPayload)) || '';
